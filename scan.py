01d8e4d3028952a08e4fe4bc095b86e5aff3da18
AntonLinderer <31562651+AntonLinderer@users.noreply.github.com>
2018-12-08 01:30:57 +1100

    updated file
    from code of https://www.pyimagesearch.com/2014/09/01/build-kick-ass-mobile-document-scanner-just-5-minutes/

--- scan.py
+++ scan.py
@@ -3,11 +3,11 @@
 
 # import the necessary packages
 from pyimagesearch.transform import four_point_transform
-from pyimagesearch import imutils
-from skimage.filters import threshold_adaptive
+from skimage.filters import threshold_local
 import numpy as np
 import argparse
 import cv2
+import imutils
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -29,16 +29,18 @@ gray = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(gray, 75, 200)
 
 # show the original image and the edge detected image
-print ("STEP 1: Edge Detection")
+print("STEP 1: Edge Detection")
 cv2.imshow("Image", image)
 cv2.imshow("Edged", edged)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # find the contours in the edged image, keeping only the
 # largest ones, and initialize the screen contour
-(_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
 cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+
 # loop over the contours
 for c in cnts:
 	# approximate the contour
@@ -52,11 +54,11 @@ for c in cnts:
 		break
 
 # show the contour (outline) of the piece of paper
-print ("STEP 2: Find contours of paper")
+print("STEP 2: Find contours of paper")
 cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
 cv2.imshow("Outline", image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # apply the four point transform to obtain a top-down
 # view of the original image
@@ -65,11 +67,11 @@ warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
 # convert the warped image to grayscale, then threshold it
 # to give it that 'black and white' paper effect
 warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-warped = threshold_adaptive(warped, 251, offset = 10)
-warped = warped.astype("uint8") * 255
+T = threshold_local(warped, 11, offset = 10, method = "gaussian")
+warped = (warped > T).astype("uint8") * 255
 
 # show the original and scanned images
-print ("STEP 3: Apply perspective transform")
+print("STEP 3: Apply perspective transform")
 cv2.imshow("Original", imutils.resize(orig, height = 650))
 cv2.imshow("Scanned", imutils.resize(warped, height = 650))
 cv2.waitKey(0)
